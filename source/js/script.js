b58cb641d837f5c7fc55a3fc4c019a80945a6c1f
Yuliya Shatokhina <59236891+YuShato@users.noreply.github.com>
2020-09-11 23:30:38 +0300

    Исправляет ошибки первой итоговой проверки наставником

--- source/js/script.js
+++ source/js/script.js
@@ -1,49 +1,50 @@
 // мобильное меню
-const menuBtn = document.querySelector('.main-nav__toggle');
-const menu = document.querySelector('.main-nav');
-const logo = document.querySelector('.page-header__logo');
+const menuBtn = document.querySelector(".page-header__toggle");
+const menu = document.querySelector(".main-nav");
+const logo = document.querySelector(".page-header__logo");
 
 
-logo.classList.remove('page-header__logo--nojs');
-menu.classList.remove('main-nav--show');
+logo.classList.remove("page-header__logo--nojs");
+menuBtn.classList.remove("page-header__toggle--nojs");
+menu.classList.remove("main-nav--show");
 
-menuBtn.addEventListener('click', function (evt) {
+menuBtn.addEventListener("click", function (evt) {
   evt.preventDefault();
-  menu.classList.toggle('main-nav--show');
-  logo.classList.toggle('page-header__logo--nojs');
-  menuBtn.classList.toggle('main-nav__toggle--open');
-  menuBtn.classList.toggle('main-nav__toggle--close');
+  menu.classList.toggle("main-nav--show");
+  logo.classList.toggle("page-header__logo--nojs");
+  menuBtn.classList.toggle("page-header__toggle--open");
+  menuBtn.classList.toggle("page-header__toggle--close");
 });
 
 // счетчик лайков
-const likes = document.querySelectorAll('.photos__like');
+const likes = document.querySelectorAll(".photos__like");
 for (let i = 0; i < likes.length; i++) {
-  likes[i].addEventListener('click', function (evt) {
+  likes[i].addEventListener("click", function (evt) {
     let counter = Number(likes[i].nextElementSibling.textContent);
-    if (likes[i].classList.contains('added')) {
+    if (likes[i].classList.contains("added")) {
       counter--;
     } else {
       counter++;
     }
-    likes[i].classList.toggle('added');
+    likes[i].classList.toggle("added");
     likes[i].nextElementSibling.textContent = counter;
   })
 };
 
 // Карта
 ymaps.ready(function () {
-  const myMap = new ymaps.Map('map', {
+  const myMap = new ymaps.Map("map", {
       center: [34.92774685612593, -111.69560981416403],
       zoom: 7,
       controls: []
     }, {
       suppressMapOpenBlock: true
     }, {
-      searchControlProvider: 'yandex#search'
+      searchControlProvider: "yandex#search"
     }),
     myPlacemark = new ymaps.Placemark(([34.92774685612593, -111.69560981416403]), {}, {
-      iconLayout: 'default#image',
-      iconImageHref: 'img/icon-map-marker.svg',
+      iconLayout: "default#image",
+      iconImageHref: "img/icon-map-marker.svg",
       iconImageSize: [27, 27],
       iconImageOffset: [0, 0]
     });
